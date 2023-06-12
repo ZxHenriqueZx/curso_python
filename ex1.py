e564a1f88300a10f29ed8529a2eeffa5a8a3aebe
ZxHenriqueZx <henriquetargaryen123@gmail.com>
2023-06-12 15:31:33 -0300

    modified:   ex1.py

--- ex1.py
+++ ex1.py
@@ -1,14 +1,24 @@
-def mult(*args):
-    total = 0 
-    for num in args:
-        multi = num 
-        multi *= num 
-        
+#EXERCÍCIO 1 
+# def mult(*args):
+#     total = 1
+#     for num in args:
+#         total *= num
 
+#     return total
 
-    print(total)
+# resultado = mult(1,8,6,4,7,2,55)
+# print(resultado)
 
+# print(1*2*8*5)
 
-mult(1,2,8,5)
-
-print(1*2*8*5)
+#EXERCÍCIO 2
+def par_impar(num):
+    resto = num % 2
+    if resto == 1:
+        return print(f'{num} é impar!')
+    return print(f'{num} é par!!')
+    
+par_impar(2)
+par_impar(5)
+par_impar(54)
+par_impar(15)
