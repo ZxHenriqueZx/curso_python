b41d09a1c328f5139988156d014a8dbbb41f50a5
ZxHenriqueZx <henriquetargaryen123@gmail.com>
2023-06-12 13:15:28 -0300

    new file:   aula6.py 	new file:   ex1.py 	modified:   "se\303\247\303\243o_1/aula36.py"

---
+++ ex1.py
@@ -0,0 +1,14 @@
+def mult(*args):
+    total = 0 
+    for num in args:
+        multi = num 
+        multi *= num 
+        
+
+
+    print(total)
+
+
+mult(1,2,8,5)
+
+print(1*2*8*5)
--- seção_1/aula36.py
+++ seção_1/aula36.py
@@ -6,5 +6,5 @@ contador = 10
 
 ###
 
-contador /= 5
+contador *= 5
 print(contador)
