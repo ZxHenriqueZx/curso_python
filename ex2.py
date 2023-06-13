24d6bdd05104fb2516db20e4336e35d96c4b4315
ZxHenriqueZx <henriquetargaryen123@gmail.com>
2023-06-13 15:59:14 -0300

    modified:   ex2.py

--- ex2.py
+++ ex2.py
@@ -2,3 +2,13 @@
 # Crie funções que duplicam, triplicam e quadruplicam
 # o número recebido como parâmetro.
 
+def duplicar(num):
+    return num * 2 
+
+def triplicar(num):
+    return num * 3
+
+def quadruplicar(num):
+    return num * 4
+
+print(quadruplicar(10))
