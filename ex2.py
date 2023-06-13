bbf9ac35f61ad90f7065b3fbc6418287b387dcc9
ZxHenriqueZx <henriquetargaryen123@gmail.com>
2023-06-13 16:01:17 -0300

    modified:   ex2.py

--- ex2.py
+++ ex2.py
@@ -12,3 +12,13 @@ def quadruplicar(num):
     return num * 4
 
 print(quadruplicar(10))
+
+# def multiplicar(numero):
+#     vezes_2 = numero * 2
+#     vezes_3 = numero * 3
+#     vezes_4 = numero * 4
+#     print(f'dobro de {numero} é {vezes_2}\n'
+#           f'triplo de {numero} é {vezes_3}\n'
+#           f'quadruplo de {numero} é {vezes_4}')
+
+# multiplicar(5)
