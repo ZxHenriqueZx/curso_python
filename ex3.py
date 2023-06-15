bbd0b9024cf22b1de7374ac2be21ff6a6d39383f
ZxHenriqueZx <henriquetargaryen123@gmail.com>
2023-06-15 17:51:46 -0300

    modified:   ex3.py

--- ex3.py
+++ ex3.py
@@ -1,20 +1,32 @@
 perguntas = [
     {
-        'Pergunta 1': 'Quanto que é 2x6 ?',
+        'Pergunta': 'Quanto que é 2x6?',
         'Opções' : ['24', '4', '8', '12'],
         'Resposta' : '12'
     },
     
     {
-        'Pergunta 2': 'Qual é a capital do Brasil',
+        'Pergunta': 'Qual é a capital do Brasil?',
         'Opções': ['Brasília', 'Rio de Janeiro', 'São Paulo', 'Brasilândia'],
         'Resposta': 'Brasília'
     }, 
 
     {
-        'Pergunta 3': 'Qual é a resposta de tudo?',
-        'Opçôes': ['Deus', 'Matemática', '42', 'James da Salada de Fruta'],
+        'Pergunta': 'Qual é a resposta de tudo?',
+        'Opções': ['Deus', 'Matemática', '42', 'James da Salada de Fruta'],
         'Resposta': '42'
     }
 ]
 
+for pergunta in perguntas:
+    print(pergunta['Pergunta'])
+    
+    for i, op in enumerate(pergunta['Opções']):
+        print(f'{i}){op}')
+
+    resposta_indice = int(input('Resposta: '))
+    
+    if pergunta['Opções'][resposta_indice] == pergunta['Resposta']:
+        print('certo')
+    
+    
