977ef6c2088c2a633d43dbf0c3d9aeb5d9ae5240
ZxHenriqueZx <henriquetargaryen123@gmail.com>
2023-06-15 16:38:25 -0300

    new file:   ex3.py

---
+++ ex3.py
@@ -0,0 +1,20 @@
+perguntas = [
+    {
+        'Pergunta 1': 'Quanto que é 2x6 ?',
+        'Opções' : ['24', '4', '8', '12'],
+        'Resposta' : '12'
+    },
+    
+    {
+        'Pergunta 2': 'Qual é a capital do Brasil',
+        'Opções': ['Brasília', 'Rio de Janeiro', 'São Paulo', 'Brasilândia'],
+        'Resposta': 'Brasília'
+    }, 
+
+    {
+        'Pergunta 3': 'Qual é a resposta de tudo?',
+        'Opçôes': ['Deus', 'Matemática', '42', 'James da Salada de Fruta'],
+        'Resposta': '42'
+    }
+]
+
