519876dd824e85597d0542a160e3164f87571c5e
ZxHenriqueZx <henriquetargaryen123@gmail.com>
2023-06-27 15:17:57 -0300

    modifiquei o arquivo termoo.py só que não terminei o projeto

--- projetos/termoo/termoo.py
+++ projetos/termoo/termoo.py
@@ -7,6 +7,14 @@ datas_dicas = json.load(dd)
 datas_random = random.choice(list(datas_dicas.keys()))
 #print(datas_dicas[datas_random])
 
+def exibir(data):
+    data_escolhida = datas_dicas[data]
+    print(30*'-')
+    print(f'Qual é a data desse acontecimento?\n{data_escolhida}')
+    print(30*'-')
+    
 print('--JOGO DE ADIVINHACÃO DE DATAS--')
- 
+exibir(datas_random)
+
 user_data = input('Data(DD/MM/AAAA): ')
+
