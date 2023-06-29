29f994aed930b0760601d7cfd3bf33fade6bf8ea
ZxHenriqueZx <henriquetargaryen123@gmail.com>
2023-06-29 17:58:41 -0300

    terminei o projeto detect_palindromo.py e criei uma pasta e um arquivo dentro dela para o proxímo projeto

--- projetos/detect_palindromo.py
+++ projetos/detect_palindromo.py
@@ -1,1 +1,42 @@
+import math
 
+palavras = ['Reviver', 'socos', 'ana', 'Luis', 'Omissíssimo', 'Carro']    
+
+#MINHA SOLUÇÃO
+def verf_palindromo(palavra):
+    palavra = palavra.upper()
+    palavra_invertida = palavra[::-1]                
+    if palavra == palavra_invertida:
+        return f'{palavra} é um palíndromo {palavra_invertida}'
+    else:
+        return f'{palavra} não é um palíndromo {palavra_invertida}'
+
+#SOLUÇÃO 1 VÍDEO
+def verf_palindromo_2(palavra):
+    palavra = palavra.upper()
+    j = len(palavra) - 1
+    score = 0
+    for i in range(len(palavra)):
+        if palavra[i] == palavra[j]:
+            score += 1
+        if i >= j:
+            break
+        j -= 1
+
+    if score == math.ceil(len(palavra) / 2):
+        return f'{palavra} é um palíndromo {palavra[::-1]}'
+    else:
+        return f'{palavra} não é um palíndromo {palavra[::-1]}'
+
+#SOLUÇÃO 2 VÍDEO
+def verf_palindromo_3(palavra):
+    palavra = palavra.upper()
+    if len(palavra) <= 1:
+        return True
+    else:
+        return palavra[0] == palavra[-1] and verf_palindromo_3(palavra[1:-1])
+        
+                
+for word in palavras:
+    print(verf_palindromo_3(word))
+    
