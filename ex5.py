d4a400c5a1a81c201a5689dcfb9039ef2ce47726
ZxHenriqueZx <henriquetargaryen123@gmail.com>
2023-07-11 18:56:27 -0300

    terminei o ex5

--- ex5.py
+++ ex5.py
@@ -1,3 +1,4 @@
+import copy
 # copy, sorted, produtos.sort
 # Exercícios
 # Aumente os preços dos produtos a seguir em 10%
@@ -10,8 +11,32 @@ produtos = [
     {'nome': 'Produto 4', 'preco': 69.90},
 ]
 
+novos_produtos = [
+    {**valor, 'preco': round(valor['preco'] * 1.1, 2)}
+    for valor in produtos
+]
+
+print(*produtos, sep='\n')
+print('')
+print(*novos_produtos, sep='\n')
+print('')
+
 # Ordene os produtos por nome decrescente (do maior para menor)
 # Gere produtos_ordenados_por_nome por deep copy (cópia profunda)
 
+produtos_ordenados_por_nome = copy.deepcopy(novos_produtos)
+produtos_ordenados_por_nome = sorted(produtos_ordenados_por_nome,
+                                     key=lambda item: item['nome'],
+                                     reverse=True)
+
+print(*produtos_ordenados_por_nome, sep='\n')
+print('')
+
 # Ordene os produtos por preco crescente (do menor para maior)
 # Gere produtos_ordenados_por_preco por deep copy (cópia profunda)
+produtos_ordenados_por_preco = copy.deepcopy(produtos_ordenados_por_nome)
+produtos_ordenados_por_preco = sorted(produtos_ordenados_por_preco,
+                                      key=lambda item: item['preco'])
+
+print(*produtos_ordenados_por_preco, sep='\n')
+
