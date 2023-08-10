27964eb09156f6a6e8ec1ac09a4a612f710b1f87
ZxHenriqueZx <henriquetargaryen123@gmail.com>
2023-08-10 11:05:31 -0300

    adicionei aula 16

---
+++ secao_3/aula016.py
@@ -0,0 +1,30 @@
+class Cliente:
+    def __init__(self, nome):
+        self.nome = nome
+        self.endereco = []
+
+    def inserir_ende(self, rua, numero):
+        self.endereco.append(Endereco(rua, numero))
+
+    def listar_enderecos(self):
+        for e in self.endereco:
+            print(e.rua, e.num)
+
+    def __del__(self):
+        print()
+        print('apagando...', self.nome)
+
+class Endereco:
+    def __init__(self, rua, num):
+        self.rua = rua
+        self.num = num
+
+    def __del__(self):
+        print()
+        print('apagando...', self.rua, self.num)
+
+
+cliente = Cliente('João')
+cliente.inserir_ende('Rua Pedro', 557)
+cliente.inserir_ende('Rua sei la', 58)
+cliente.listar_enderecos()
