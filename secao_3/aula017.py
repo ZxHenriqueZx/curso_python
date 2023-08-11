259f5f6b6db414704993137c6e6b253accedb237
ZxHenriqueZx <henriquetargaryen123@gmail.com>
2023-08-11 12:10:12 -0300

    modifiquei aula017.py

--- secao_3/aula017.py
+++ secao_3/aula017.py
@@ -8,4 +8,19 @@
 #   -> super class, base class, parent class
 # Classes filhas (Cliente)
 #   -> sub class, child class, derived class
+class Pessoa:
+    def __init__(self, nome, sobrenome):
+        self.nome = nome
+        self.sobrenome = sobrenome
 
+    def falar_oi(self):
+        print(self.nome, self.sobrenome, self.__class__.__name__)
+
+class Cliente(Pessoa):
+    ...
+
+class Aluno(Pessoa):
+    ...
+
+c1 = Cliente('Luis', 'Henrique')
+a1 = Aluno('Pedro', 'Henrique')
