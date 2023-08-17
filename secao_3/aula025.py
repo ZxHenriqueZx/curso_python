508b82dc70b4ccc05835a95a164029225a4d9135
ZxHenriqueZx <henriquetargaryen123@gmail.com>
2023-08-17 14:59:05 -0300

    modified: aula025.py

--- secao_3/aula025.py
+++ secao_3/aula025.py
@@ -15,3 +15,22 @@
 # __neg__(self) - -self
 # __str__(self) - str
 # __repr__(self) - str
+class Ponto:
+    def __init__(self, x, y, z='STRING'):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __str__(self):
+        return f'{self.x}, {self.y}'
+
+    def __repr__(self):
+        class_name = type(self).__name__
+        return f'{class_name}(x={self.x!r}, y={self.y!r}, z={self.z!r})'
+
+p1 = Ponto('5', 9)
+p2 = Ponto(43, '56')
+print(p1)
+print(p2)
+print(repr(p1))
+
