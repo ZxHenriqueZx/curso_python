24efa0f8e24949205622a78dc5e00b4132eb25d1
ZxHenriqueZx <henriquetargaryen123@gmail.com>
2023-08-18 17:02:54 -0300

    modified:   aula028.py 	modified:   aula028.txt

--- secao_3/aula028.py
+++ secao_3/aula028.py
@@ -18,26 +18,33 @@
 # Ex:
 # with open('aula149.txt', 'w') as arquivo:
 #     ...
-class MyContextManager:
+class MyOpen:
     def __init__(self, caminho_arquivo, modo):
-        print('INIT')
-        self.caminho = caminho_arquivo
+        self.caminho_arquivo = caminho_arquivo
         self.modo = modo
         self._arquivo = None
-
-
     def __enter__(self):
-        print('ABRIU')
-        self._arquivo = open(self.caminho, self.modo)
+        print('ABRINDO ARQUIVO')
+        self._arquivo = open(self.caminho_arquivo, self.modo, encoding='utf8')
         return self._arquivo
-
-    def __exit__(self, class_ex, exc, tracba):
-        print('FECHOU')
+    def __exit__(self, class_exception, exception_, traceback_):
+        print('FECHANDO ARQUIVO')
         self._arquivo.close()
 
-my_open = MyContextManager('aula028.txt', 'w')
+        # raise class_exception(*exception_.args).with_traceback(traceback_)
 
-with my_open as arquivo:
-    arquivo.write('Linha1\n')
-    arquivo.write('Linha2\n')
+        # print(class_exception)
+        # print(exception_)
+        # print(traceback_)
+        # exception_.add_note('Minha nota')
+
+        # return True  # Tratei a exceção
+
+
+with MyOpen('aula149.txt', 'w') as arquivo:
+    arquivo.write('Linha 1\n')
+    arquivo.write('Linha 2\n')
+    arquivo.write('Linha 2\n', 123)
+    arquivo.write('Linha 3\n')
+    print('WITH', arquivo)
 
