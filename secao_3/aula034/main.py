edf12c56be6df40b3e51fe82fa0c14e4e8844ab3
ZxHenriqueZx <henriquetargaryen123@gmail.com>
2023-08-28 15:49:14 -0300

    modificado módulo aula034 e adicionado arquivo documentando_class

--- secao_3/aula034/main.py
+++ secao_3/aula034/main.py
@@ -1,10 +1,11 @@
 #import uma_linha
 #import varias_linhas
-import documentando_funcao
+#import documentando_funcao
+import documentando_class
 
 #print(dir(uma_linha))
 #print(uma_linha.__name__)
 #print(uma_linha.__doc__)
 #print(uma_linha.__file__)
 
-help(documentando_funcao)
+help(documentando_class)
