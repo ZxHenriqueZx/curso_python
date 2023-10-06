549ce7144354df7095f8a74ce1818e5644afaaf1
ZxHenriqueZx <henriquetargaryen123@gmail.com>
2023-10-06 11:50:36 -0300

    modified:   aula037/main.py 	new file:   aula037/workbook.xlsx

--- secao_4/aula037/main.py
+++ secao_4/aula037/main.py
@@ -7,4 +7,35 @@
 # manipulação de grandes quantidades de informações.
 # Instalação necessária: pip install openpyxl
 # Documentação: https://openpyxl.readthedocs.io/en/stable/
+from pathlib import Path
+from openpyxl import Workbook
+from openpyxl.worksheet.worksheet import Worksheet
+
+ROOT_FOLDER = Path(__file__).parent
+WORKBOOK_PATH = ROOT_FOLDER / 'workbook.xlsx'
+
+workbook = Workbook()
+worksheet = workbook.active
+
+worksheet.cell(1, 1, 'Nome')
+worksheet.cell(1, 2, 'Idade')
+worksheet.cell(1, 3, 'Nota')
+
+students = [
+
+    ['Luis', 18, 6.8],
+    ['Pedro', 28, 8.6],
+    ['João', 15, 9.0],
+    ['Jamal', 19, 5.0]
+
+]
+
+#for i, stu in enumerate(students, start=2):
+#    for j, st in enumerate(stu, start=1):
+#        worksheet.cell(i, j, st)
+
+for student in students:
+    worksheet.append(student)
+
+workbook.save(WORKBOOK_PATH)
 
