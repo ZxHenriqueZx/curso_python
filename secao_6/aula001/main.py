ed2d72f059d4f483b951190a3332acb20629b0fe
ZxHenriqueZx <henriquetargaryen123@gmail.com>
2023-10-23 11:22:06 -0300

    adicionando a primeira coluna no arquivo sqlite3

--- secao_6/aula001/main.py
+++ secao_6/aula001/main.py
@@ -7,6 +7,15 @@ DB_FILE = ROOT_DIR / 'db.sqlite3'
 connection = sqlite3.connect(DB_FILE)
 cursor = connection.cursor()
 
+cursor.execute(
+    'CREATE TABLE IF NOT EXISTS customers'    
+    '('
+    'id INTEGER PRIMARY KEY AUTOINCREMENT,'
+    'name TEXT,'
+    'weight REAL'
+    ')'
+)
+connection.commit()
 
 cursor.close()
 connection.close()
