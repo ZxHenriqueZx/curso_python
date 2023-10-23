e58a9710d2a99355234e0d7c61444f85001ec222
ZxHenriqueZx <henriquetargaryen123@gmail.com>
2023-10-23 14:47:47 -0300

    adicionando os primeiros dados ao arquivo db.sqlite3

--- secao_6/aula001/main.py
+++ secao_6/aula001/main.py
@@ -8,12 +8,29 @@ connection = sqlite3.connect(DB_FILE)
 cursor = connection.cursor()
 
 cursor.execute(
+    'DELETE FROM customers'
+)
+connection.commit()
+
+cursor.execute(
+    'DELETE FROM sqlite_sequence WHERE name="{customers}"'
+)
+connection.commit()
+
+cursor.execute(
     'CREATE TABLE IF NOT EXISTS customers'    
     '('
     'id INTEGER PRIMARY KEY AUTOINCREMENT,'
     'name TEXT,'
     'weight REAL'
     ')'
+)
+connection.commit()
+
+cursor.execute(
+    'INSERT INTO customers (id, name, weight) '
+    'VALUES (NULL, "Pedro", 2.80),'
+    '(NULL, "Luis Henrique", 1.80)'
 )
 connection.commit()
 
