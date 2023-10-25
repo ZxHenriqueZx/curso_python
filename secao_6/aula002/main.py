95322bc707a558a5d0a45a462bff022bbe15065e
ZxHenriqueZx <henriquetargaryen123@gmail.com>
2023-10-25 14:58:21 -0300

    new file:   main.py 	Fiz a conexão com o servidor mysql

---
+++ secao_6/aula002/main.py
@@ -0,0 +1,20 @@
+# PyMySQL - um cliente MySQL feito em Python Puro
+# Doc: https://pymysql.readthedocs.io/en/latest/
+# Pypy: https://pypi.org/project/pymysql/
+# GitHub: https://github.com/PyMySQL/PyMySQL
+import pymysql
+import dotenv
+import os
+
+dotenv.load_dotenv()
+
+connection = pymysql.connect(
+    host=os.environ['MYSQL_HOST'],
+    user=os.environ['MYSQL_USER'],
+    password=os.environ['MYSQL_PASSWORD'],
+    database=os.environ['MYSQL_DATABASE']
+)
+
+with connection:
+    with connection.cursor() as cursor:
+        ...
