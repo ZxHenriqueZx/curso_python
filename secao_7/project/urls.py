087ae2db1857596cba72f0036f8a2453b70a4bda
ZxHenriqueZx <henriquetargaryen123@gmail.com>
2023-10-31 17:43:27 -0300

    modified:   project/urls.py 	Configurei a url home

--- secao_7/project/urls.py
+++ secao_7/project/urls.py
@@ -18,10 +18,14 @@ from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
 
-def my_view(request):
-    return HttpResponse('Hello World')
+def home(request):
+    return HttpResponse('Home')
+
+def blog(request):
+    return HttpResponse('Blog')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', my_view),
+    path('', home),
+    path('blog/', blog),
 ]
